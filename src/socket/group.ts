05f402907d24b71ad90754635dc50feef02ba02a
Infatuation <5252107+infatuation5858@user.noreply.gitee.com>
2023-12-18 21:01:46 +0800

    commit: complier

--- src/socket/group.ts
+++ src/socket/group.ts
@@ -4,14 +4,14 @@ import { Message, MessageOrigin, MessageType } from "../types/chat";
 // 群聊
 
 export default function joinGroup(socket: Socket) {
-  socket.join("chat room");
-  socket.on("sendMessage", (data) => {
-    console.log(data, "收到消息啦");
-    const message: Message = {
-      type: MessageType.TEXT,
-      data: data?.message,
-      time: new Date().getTime(),
-    };
-    socket.to("chat room").emit("receiveMessage", message);
-  });
+  // socket.join("chat room");
+  // socket.on("sendMessage", (data) => {
+  //   console.log(data, "收到消息啦");
+  //   const message: Message = {
+  //     type: MessageType.TEXT,
+  //     data: data?.message,
+  //     time: new Date().getTime(),
+  //   };
+  //   socket.to("chat room").emit("receiveMessage", message);
+  // });
 };
